35a9b21e14eb9ec7eda5be0ab1e4bffb0d1d1808
serp0v <serpov.cc@mail.ru>
2022-09-14 00:31:30 +1000

    add balance to card operation

--- main.js
+++ main.js
@@ -107,19 +107,19 @@ const cardToBalanceOperation = document.getElementById('cardToBalanceOperation')
   }
 }
 
-// const balanceToCardOperation = document.getElementById('balanceToCardOperation').onclick = () => {
-//   const balanceToCard = document.getElementById('balanceToCard').value;
-//   console.log(balanceToCard);
-//   if (balanceToCard <= balanceCount) {
-//     balanceCount = balanceCount - +balanceToCard;
-//     bankCardCount = bankCardCount + +balanceToCard;
-//     bankCard.innerHTML ="На карте: " + bankCardCount + "$";
-//     balance.innerHTML ="Карманные деньги: " + balanceCount + "$";
-//   }
-//   if (cardToBalance > balanceCount) {
-//     console.log('error');
-//   }
-// }
+const balanceToCardOperation = document.getElementById('balanceToCardOperation').onclick = () => {
+  const balanceToCard = document.getElementById('balanceToCard').value;
+  console.log(balanceToCard);
+  if (balanceToCard <= balanceCount) {
+    balanceCount = balanceCount - +balanceToCard;
+    bankCardCount = bankCardCount + +balanceToCard;
+    bankCard.innerHTML ="На карте: " + bankCardCount + "$";
+    balance.innerHTML ="Карманные деньги: " + balanceCount + "$";
+  }
+  if (cardToBalance > balanceCount) {
+    console.log('error');
+  }
+}
 
 // Уровень
 
